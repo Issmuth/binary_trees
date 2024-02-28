faa3757c4951230cdbab562afa50e85ae655009e
Issmuth <Ismael.mdev@gmail.com>
2024-02-29 00:10:35 +0300

    done

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- *
+ * binary_tree_is_root - checks if a node is a root node
+ * @node: node to be checked
  */
 
 int binary_tree_is_root(const binary_tree_t *node)
